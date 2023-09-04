44a8255c8b052e381c4045477744b15baa11f970
miguel <mreynoso@tapterminal.com>
2023-09-04 16:25:09 -0600

    95%

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -5,7 +5,8 @@
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <style type="text/css">
         @page {
-            margin-top: 30px;
+            margin-top: 80px !important;
+            margin-bottom: 50px !important;
             max: height 800px;
         }
         * { padding: 0; margin: 0; }
@@ -120,69 +121,88 @@
         .tablaNota {
             padding-top: 10px;
         }
+        #watermark {
+            margin-top: -80px !important;
+            position: fixed;
+            top:   0px;
+            left:  0px;
+            /** The width and height may change 
+                according to the dimensions of your letterhead
+            **/
+            width:    790px;
+            height:   950px;
+
+            /** Your watermark should be behind every content**/
+            z-index:  -1000;
+        }
+
+        .encabezado-pagina-adicional {
+            margin-top: 30px; /* Ajusta la cantidad de espacio según tus necesidades */
+        }
     </style>
 </head>
 <body >
-    <!-- <div class="header-info"> Refis360 Móvil </div>
-    <div class="header-info"> Comercializadora La Junta S.A. de C.V. </div> -->
-    <br>
-    <div style="text-align: center; font-size: 10pt;padding-top: 40px;"><strong>Detalles de Carga</strong></div>
+    <div id="watermark">
+        <img src="https://refis360movildev.s3.us-west-1.amazonaws.com/PlantillasImg/PlantillaPDF_1.png" height="100%" width="100%" />
+    </div>
+
+    <div style="text-align: center; font-size: 10pt;"><strong>Detalles de Carga</strong></div>
     
-    <div class='main' style="page-break-after:always;">
+    <div class='main' >
         <table class='TablaDetallesEntrada'>
             <tr>
                 <td class="titulo"><strong>Entrada</strong></td>
-                <td style='color : red;'>asasd</td>
+                <td style='color : red;'>{{$entrada}}</td>
                 <td class="titulo"><strong>Referencia</strong></td>
-                <td>asasd</td>
+                <td>{{$refenrencia}}</td>
                 <td class="titulo"><strong>F. de Entrada</strong></td>
-                <td>saasdadsasd</td>
+                <td>{{$fechaEntrada}}</td>
             </tr>
             <tr>
                 <td class="titulo"><strong>Contenedor</strong></td>
-                <td>asasd</td>
+                <td>{{$Contenedor}}</td>
                 <td class="titulo"><strong>&Aacute;rea</strong></td>
-                <td>asasd</td>
+                <td>{{$area}}</td>
                 <td class="titulo"><strong>Sello</strong></td>
             </tr>
             <tr>
                 <td class="titulo"><strong>Buque(Viaje)</strong></td>
-                <td>asasdasasdasdasdadasdas</td>
+                <td>{{$buqueViaje}}</td>
                 <td class="titulo"><strong>F. Arribo</strong></td>
-                <td>asasd</td>
+                <td>{{$fechaArribo}}</td>
             </tr>
             <tr>
                 <td class="titulo"><strong>Puerto de <br> Procedencia</strong></td>
-                <td>asasd</td>
+                <td>{{$puertoProcedencia}}</td>
                 <td class="titulo"><strong>L&iacute;nea Naviera</strong></td>
-                 <td>estaEsUnaLinea <!--NavieraQue ExcedeElAncho --></td>
+                <td>{{$lineaNaviera}}</td>
                 <td></td>
                 <td></td>
             </tr>
             <tr>
                 <td class="titulo"><strong>Patente</strong></td>
-                <td>asasd</td>
+                <td>{{$patente}}</td>
                 <td class="titulo"><strong>Agencia Aduanal</strong></td>
-                <td>asasd</td>
+                <td>{{$agenciaAduanal}}</td>
             </tr>
             <tr>
                 <td class="titulo"><strong>Manifiesto</strong></td>
-                <td>asasd</td>
+                <td>{{$Manifiesto}}</td>
                 <td class="titulo"><strong>BL</strong></td>
-                <td>asasd</td>
+                <td>{{$BL}}</td>
             </tr>
             <tr>
                 <td><strong>F. Inicio <br> desconsolidación</strong></td>
-                <td>asasd</td>
-                <td><strong>F. Termino <br> desconsolidación</strong></td>
-                <td>asasd</td>
+                <td>{{$fehcaInicioD}}</td>
+                <td><strong>F. Término <br> desconsolidación</strong></td>
+                <td>{{$fTerminoD}}</td>
             </tr>
         </table>
     </div>
 
     <div class="main" >
-        <table class="TablaDetallesMercancias" style="padding-top: 80px">
-            <thead style="border-bottom: 2px solid;">
+        <table class="TablaDetallesMercancias" >
+            <thead style="border-bottom: 2px solid; padding-top: 80px;">
                 <tr>
                     <th>Entrada</th>
                     <th>HBL</th>
@@ -202,7 +222,7 @@
                 </tr>
             </thead>
             <tbody>
-                @foreach($datos as $detalle)
+                @foreach($datosTabla as $detalle)
                 <tr class="DetalleMercancia">
                     <td>{{ $detalle['Entrada'] }}</td>
                     <td>{{ $detalle['HBL'] }}</td>
@@ -226,7 +246,7 @@
     </div>
 
     <div class="main">
-        <table class="tablaNota">
+        <table class="tablaNota" >
             <tr>
                 <td class='CeldaNota' nowrap style='word-wrap: break-word; word-break: break-all;'>
                     <div >
